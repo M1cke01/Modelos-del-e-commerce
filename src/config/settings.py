81463e8901d635fd8d131125b0e40e5b383dc47f
M1cke01 <miguelreyes1940145@gmail.com>
2025-06-04 13:07:30 -0600

    primer commit

--- src/config/settings.py
+++ src/config/settings.py
@@ -35,6 +35,13 @@ ALLOWED_HOSTS = list(map(str.strip, allowed_hosts.split(",")))
 # Application definitions
 INSTALLED_APPS = [
     "pages.apps.PagesConfig",
+    "analytics.apps.AnalyticsConfig",
+    "accounts.apps.AccountsConfig",
+    "address.apps.AddressConfig",
+    "billing.apps.BillingConfig",
+    "carts.apps.CartsConfig",
+    "order.apps.OrderConfig",
+    "products.apps.ProductsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
