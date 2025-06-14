7aad00cffffdbbd2fff3e48afbd1e2c080d10d11
Miguel Reyes <120216237+M1cke01@users.noreply.github.com>
2025-06-14 10:30:42 -0600

    Update models.py

--- src/products/models.py
+++ src/products/models.py
@@ -1,6 +1,6 @@
 import os
-from decimal import Decimal
-from django.conf import settings
+#from decimal import Decimal
+#from django.conf import settings
 from django.db import models
 #from django.db.models.signals import pre_save, post_save
 
